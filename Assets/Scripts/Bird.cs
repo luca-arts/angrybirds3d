10f4aeee9bfa3b81d338d9a60c24e4a8354ea00a
wouter.devriese@luca-arts.be <wouter.devriese@luca-arts.be>
2022-09-07 18:33:37 +0200

    update for trajectory showing

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -15,18 +15,84 @@ public class Bird : MonoBehaviour
     private bool _isPressed;
     private bool _isFired;
 
+    [Header("Display Controls")]
+    [SerializeField]
+    [Range(10, 100)]
+    private int LinePoints = 25;
+    [SerializeField]
+    [Range(0.01f, 0.25f)]
+    private float TimeBetweenPoints = 0.1f;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private SpringJoint springJoint;
+
+    [SerializeField] private Transform slingShot;
+    [SerializeField] private Transform hook;
+    private void Start()
+    {
+        foreach (Transform GO in slingShot.GetComponentsInChildren<Transform>(true))
+        {
+            Debug.Log(GO.name);
+            if (GO.name == "Hook")
+            {
+                hook = GO;
+            }
+        }
+    }
+
     void FixedUpdate()
     {
         if (_isPressed && !_isFired && !GameManager.Instance.IsLevelCleared)
         {
             Vector3 mousePosition = Input.mousePosition;
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 6.5f));
+            DrawProjection(worldPosition);
+            //next lines are to make sure you can grab the bird
             if (worldPosition.y >= 0.2f && worldPosition.y <= 8f)
             {
                 Rb.position = worldPosition;
             }
         }
     }
+
+
+    private void DrawProjection(Vector3 ReleasePosition)
+    {
+        
+        if (hook != null)
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.positionCount = Mathf.CeilToInt(LinePoints / TimeBetweenPoints) + 1;
+            Vector3 startPosition = ReleasePosition;
+            Vector3 _direction = hook.transform.position - startPosition;
+            //Vector3 startVelocity = springJoint.spring * _direction / Rb.mass;
+            Vector3 startVelocity = Mathf.Sqrt(springJoint.spring) * _direction/ Rb.mass;
+
+            int i = 0;
+            lineRenderer.SetPosition(i, startPosition);
+            for (float time = 0; time < LinePoints; time += TimeBetweenPoints)
+            {
+                i++;
+                Vector3 point = startPosition + time * startVelocity;
+                point.y = startPosition.y + startVelocity.y * time + (Physics.gravity.y / 2f * time * time) - springJoint.damper * time;
+
+                lineRenderer.SetPosition(i, point);
+
+                Vector3 lastPosition = lineRenderer.GetPosition(i - 1);
+
+                if (Physics.Raycast(lastPosition,
+                    (point - lastPosition).normalized,
+                    out RaycastHit hit,
+                    (point - lastPosition).magnitude,
+                    GrenadeCollisionMask))
+                {
+                    lineRenderer.SetPosition(i, hit.point);
+                    lineRenderer.positionCount = i + 1;
+                    return;
+                }
+            }
+        }
+    }
+
 
     void OnMouseDown()
     {
